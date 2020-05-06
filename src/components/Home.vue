4522c97ce170cac52e368354bf3d6a7e1467a551
Wendel Silva <wfrsilva@gmail.com>
2020-05-05 23:41:23 -0300

    Ate 07 minutos - Ping pong OK - Projeto Electron 06/10 : Utilizando Electron IPC (Inter-Process Commu…
    Ate 07 minutos - Ping pong OK
    blabla para blabla
    Projeto Electron 06/10 : Utilizando Electron IPC (Inter-Process Commu…

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,57 @@
 <template>
   <v-container fluid>
-     <v-form>
-         <v-file-input 
-         label = "Selecione as Legendas"
-         prepend-icon= "mdi-message-text"
-         append-outer-icon="mdi-send"
-         outlined
-         multiple 
-         chips 
-         v-model="files"
-         @click:append-outer="processSubtitles"
-         
-         />
-             
-     </v-form>
+    <v-form>
+      <v-file-input
+        label="Selecione as Legendas"
+        prepend-icon="mdi-message-text"
+        append-outer-icon="mdi-send"
+        outlined
+        multiple
+        chips
+        v-model="files"
+        @click:append-outer="processSubtitles"
+      />
+    </v-form>
 
     <div clas="pills">
       <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount" />
     </div>
   </v-container>
 </template>
+
+<script>
+import { ipcRenderer } from "electron";
+import Pill from "./Pill";
+export default {
+  components: { Pill },
+  data: function() {
+    return {
+      files: [],
+      groupedWords: [
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+        { name: "me", amount: 999 },
+      ]
+    };
+  },
+  methods: {
+    processSubtitles() {
+      console.log(this.files);
+
+      ipcRenderer.send('blabla', 'ping do Home')
+      ipcRenderer.on('blabla', (event, resp) => {
+        console.log(resp)
+      })
+    }
+  }
+};
+</script>
+
+<style >
+  .pills {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+</style>
